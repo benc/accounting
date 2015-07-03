115062a191da280c91bf334dd40bbb90f4c6850a
Ben Cochez <ben@cochez.info>
2015-07-03 11:33:56 +0200

    more elegant

--- client/src/app/services/invoiceService.ts
+++ client/src/app/services/invoiceService.ts
@@ -1,13 +1,11 @@
 import { bind, Inject, Http } from 'angular2/angular2';
 
 export class InvoiceService {
-  http: Http;
-  
-  constructor(@Inject(Http) http: Http) {
+  constructor(@Inject(Http) public http: Http) {
     // was an angular 1.x resource pointing at 'http://localhost:3000/api/invoice/:id'
     this.http = http;
   }
-  
+
   getInvoices() {
     return this.http.get('http://localhost:3000/api/invoice');
   }
