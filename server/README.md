a820cbb4bc29f7ff0a340e148e7f58c8f9b8b78b
Ben Cochez <ben@cochez.info>
2015-08-10 20:46:25 +0200

    no more rails

--- server/README.md
+++
@@ -1,7 +0,0 @@
-# README
-
-Sample backend app using rails-api and grape.
-
-## Get it up and running
-
-Checkout the main README
--- server/db/migrate/20141115175516_add_currency_to_invoice.rb
+++
@@ -1,5 +0,0 @@
-class AddCurrencyToInvoice < ActiveRecord::Migration
-  def change
-    add_column :invoices, :currency, :string # ISO 4217
-  end
-end
